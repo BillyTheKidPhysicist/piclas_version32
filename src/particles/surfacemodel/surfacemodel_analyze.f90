cd0e9220e27c49bf3b7db94e78dd56f9ce0b3960
William Huntington <williamhuntington@Williams-MacBook-Pro.local>
2024-04-29 10:33:00 +0200

    changes

--- src/particles/surfacemodel/surfacemodel_analyze.f90
+++ src/particles/surfacemodel/surfacemodel_analyze.f90
@@ -371,7 +371,7 @@ IF(MPIRoot)THEN
           !billy
           !broadcast the new yield to everyone
 #if USE_MPI 
-              print *, 'yield updated', SEE%SurfModEmissionYield
+              print *, 'yield updated', SEE%SurfModEmissionYield,total_current,SEE%MaximumCurrent
               CALL MPI_BCAST(SEE%SurfModEmissionYield,1, MPI_DOUBLE_PRECISION,0,SurfCOMM%UNICATOR,iERROR)
               !CALL MPI_BCAST(SEE%total_current,1, MPI_DOUBLE_PRECISION,0,SurfCOMM%UNICATOR,iERROR)
               !CALL MPI_COMM_RANK(MPI_COMM_WORLD, rank, ierror) !for printing I need the rank
