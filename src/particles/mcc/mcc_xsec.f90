ec80130906ff225f7ab81401b1ace80b91e97957
William Huntington <williamhuntington@Williams-MacBook-Pro.local>
2024-05-16 14:44:01 +0200

    changes

--- src/particles/mcc/mcc_xsec.f90
+++ src/particles/mcc/mcc_xsec.f90
@@ -516,6 +516,9 @@ IF(SpecXSec(iCase)%UseCollXSec) THEN
   CollEnergy = 0.5 * ReducedMassUnweighted * Coll_pData(iPair)%CRela2
   ! Calculate the collision probability
   SpecXSec(iCase)%CrossSection = InterpolateCrossSection(SpecXSec(iCase)%CollXSecData,CollEnergy)
+
+  !billy
+  print *, SpecXSec(iCase)%CrossSection, CollEnergy,ReducedMassUnweighted,Coll_pData(iPair)%CRela2
   Coll_pData(iPair)%Prob = (1. - EXP(-SQRT(Coll_pData(iPair)%CRela2) * SpecXSec(iCase)%CrossSection * SpecNumTarget * MacroParticleFactor &
                                         / Volume * dtCell))
   ! Correction for conditional probabilities in case of MCC
